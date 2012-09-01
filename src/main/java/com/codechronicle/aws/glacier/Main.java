f62235a60bae34b610daf38da02f76c50da9f8e3
Saptarshi Roy <saptarshi.roy@salesforce.com>
2012-09-01 13:16:53 -0700

    Initial commit, app skeleton

---
+++ src/main/java/com/codechronicle/aws/glacier/Main.java
@@ -0,0 +1,18 @@
+package com.codechronicle.aws.glacier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Main {
+
+    private static Logger log = LoggerFactory.getLogger(Main.class);
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+        log.info("Hello world");
+	}
+
+}
---
+++ src/test/java/com/codechronicle/aws/glacier/MainTest.java
@@ -0,0 +1,12 @@
+package com.codechronicle.aws.glacier;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
