ecb8447c3b8514e973beda17c261967271bc4e2f
Saptarshi Roy <saptarshi.roy@salesforce.com>
2012-09-06 14:02:35 -0700

    Enhanced tests to generate their own test files

--- src/main/java/com/codechronicle/aws/glacier/Main.java
+++ src/main/java/com/codechronicle/aws/glacier/Main.java
@@ -1,125 +1,16 @@
 package com.codechronicle.aws.glacier;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.PropertiesCredentials;
-import com.amazonaws.services.glacier.AmazonGlacierClient;
-import com.amazonaws.services.glacier.model.*;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
-import java.util.List;
-import java.util.Properties;
+import java.io.IOException;
 
 
 public class Main {
 
     private static Logger log = LoggerFactory.getLogger(Main.class);
 
-    private static int ONE_MEGABYTE = 1024 * 1024;
-    private static int PART_SIZE = ONE_MEGABYTE * 16;
-
     public static void main(String[] args) throws IOException, FilePartException {
-
+        System.out.println("Hello world");
     }
-
-    public static void mainX(String[] args) {
-        File f = new File("/home/saptarshi.roy/Downloads/ubuntu-10.04.4-server-amd64.iso");
-
-        // First, decide on part size
-        if (f.length() < PART_SIZE) {
-            // Just upload it all at once
-        } else {
-            // Do a multi-part upload
-            uploadMultiPart(f);
-        }
-
-        System.out.println(f.length());
-    }
-
-    private static void uploadMultiPart(File f) {
-        int numParts = (int)(f.length() / PART_SIZE);
-        System.out.println("Num parts = " + numParts);
-        byte[] buffer = new byte[PART_SIZE];
-
-        RandomAccessFile raf = null;
-        FileOutputStream os = null;
-        try {
-            File outfile = new File("/tmp/testOutput.txt");
-            os = new FileOutputStream(outfile);
-
-            raf = new RandomAccessFile(f, "r");
-
-            for (int i=0; i<numParts; i++) {
-                System.out.println("Part " + i + " = " + i);
-                raf.seek(i * PART_SIZE);
-                raf.read(buffer);
-                os.write(buffer);
-            }
-
-            int finalPartStart = (numParts * PART_SIZE);
-            long finalPartSize = f.length() - finalPartStart;
-            raf.seek(finalPartStart);
-            raf.read(buffer, 0, (int)finalPartSize);
-            os.write(buffer, 0, (int)finalPartSize);
-
-        } catch (Exception e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        } finally {
-            if (raf != null) IOUtils.closeQuietly(raf);
-            if (os != null) IOUtils.closeQuietly(os);
-        }
-    }
-
-    private static void uploadPart(RandomAccessFile raf, int partNum, int partSize) {
-
-        long filePointerLocation = partNum * partSize;
-
-    }
-
-    /**
-	 * @param args
-	 */
-	public static void testListVaults(String[] args) throws Exception {
-
-        Properties props = loadAWSCredentials();
-        AWSCredentials credentials = new BasicAWSCredentials(props.getProperty("accessKey"), props.getProperty("secretKey"));
-
-        AmazonGlacierClient agc = new AmazonGlacierClient(credentials);
-        agc.setEndpoint(props.getProperty("endPoint"));
-
-//        CreateVaultRequest cvRequest = new CreateVaultRequest(props.getProperty("accountId"), "PersonalMedia");
-//        CreateVaultResult cvResult = agc.createVault(cvRequest);
-//        System.out.println("Location = " + cvResult.getLocation());
-
-        ListVaultsRequest lvRequest = new ListVaultsRequest(props.getProperty("accountId"));
-        ListVaultsResult lvResult = agc.listVaults(lvRequest);
-        List<DescribeVaultOutput> vaultDescriptions = lvResult.getVaultList();
-        for (DescribeVaultOutput vaultDescription : vaultDescriptions) {
-            System.out.println(vaultDescription.getVaultName() + " -- " + vaultDescription.getCreationDate());
-        }
-	}
-
-    private static Properties loadAWSCredentials() {
-
-        String userHome = System.getenv("HOME");
-        File credFile = new File(userHome + "/.aws/aws.properties");
-
-        Properties props = new Properties();
-        FileReader fr = null;
-
-        try {
-            fr = new FileReader(credFile);
-            props.load(fr);
-            return props;
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        } finally {
-            IOUtils.closeQuietly(fr);
-        }
-    }
-
 }
--- src/test/java/com/codechronicle/aws/glacier/FileOperationSplitterTest.java
+++ src/test/java/com/codechronicle/aws/glacier/FileOperationSplitterTest.java
@@ -1,42 +1,116 @@
 package com.codechronicle.aws.glacier;
 
+import com.amazonaws.services.glacier.TreeHashGenerator;
+import junit.framework.Assert;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-
-import static org.testng.Assert.*;
 
 public class FileOperationSplitterTest {
 
-    private static int ONE_MEGABYTE = 1024 * 1024;
-    private static int PART_SIZE = ONE_MEGABYTE * 16;
+    private static int PART_SIZE = 4096;
 
-    //TODO: Generation and cleanup of test files
-    //TODO: Hash based comparison to ensure the files are exactly the same
+    private File inputFileWithRemainder;
+    private File inputFileWithPrecisePartitionSizeMultiple;
+
     //TODO: Insert mechanism for aborting midstream and then continuing where it left off
 
-    @Test
-    public void testSmallFileCopy() throws IOException, FilePartException {
-        File f = new File("/tmp/testShortInput.txt");
-        FileOperationSplitter fos = new FileOperationSplitter("testjob", f, 100);
+    @BeforeSuite
+    public void setup() {
+        // Create test files
+        try {
+            inputFileWithRemainder = File.createTempFile("withRemainder", ".dat");
+            inputFileWithPrecisePartitionSizeMultiple = File.createTempFile("precisePartitionMultiple", ".dat");
 
-        final File outfile = new File("/tmp/testSmallOutput.txt");
+            writeFileOfSize(inputFileWithRemainder, (int)(PART_SIZE * 10.5));
+            writeFileOfSize(inputFileWithPrecisePartitionSizeMultiple, PART_SIZE * 10);
 
-        fos.setFpOperator(new FileCopyOperator(outfile));
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
 
-        fos.start();
+    private void writeFileOfSize(File outputFile, int numBytes) throws IOException {
+        BufferedOutputStream bos = null;
+
+        try {
+            bos = new BufferedOutputStream(FileUtils.openOutputStream(outputFile));
+
+            byte b = (byte)(255 * Math.random());
+            for (int i=0; i<numBytes; i++) {
+                bos.write(b);
+            }
+
+        } catch (IOException ie) {
+            throw ie;
+        } finally {
+            if (bos != null) {
+                IOUtils.closeQuietly(bos);
+            }
+        }
+    }
+
+    public void testCopyFile(String jobId, File srcFile) throws IOException, FilePartException {
+        FileOperationSplitter fos = new FileOperationSplitter(jobId, srcFile, PART_SIZE);
+
+        File outFile = null;
+
+        try {
+
+            outFile = File.createTempFile(jobId, ".out");
+
+            FileCopyOperator copyOperator = new FileCopyOperator(outFile);
+            fos.setFpOperator(copyOperator);
+            fos.start();
+
+            System.out.println(srcFile.getAbsolutePath() + " = " + srcFile.length() + " bytes");
+            System.out.println(outFile.getAbsolutePath() + " = " + outFile.length() + " bytes");
+
+            // Compare the two files
+            long srcHash = FileUtils.checksumCRC32(srcFile);
+            long outHash = FileUtils.checksumCRC32(outFile);
+
+            System.out.println("Source Hash = " + srcHash);
+            System.out.println("Output Hash = " + outHash);
+
+            Assert.assertEquals(srcHash, outHash);
+
+        } catch (IOException io) {
+            throw io;
+        } finally {
+            if (outFile != null) {
+                outFile.delete();
+            }
+        }
     }
 
     @Test
-    public void testLargeFileCopy() throws IOException, FilePartException {
-        File f = new File("/tmp/testInput.txt");
-        FileOperationSplitter fos = new FileOperationSplitter("testjob", f, PART_SIZE);
+    public void testFileWithRemainder () throws IOException, FilePartException {
+        testCopyFile("testFileWithRemainder", inputFileWithRemainder);
+    }
 
-        final File outfile = new File("/tmp/testLargeOutput.txt");
+    @Test
+    public void testFileWithPreciseSizeMultiple () throws IOException, FilePartException {
+        testCopyFile("testFileWithPrecisePartitionSizeMultiple", inputFileWithPrecisePartitionSizeMultiple);
+    }
 
-        fos.setFpOperator(new FileCopyOperator(outfile));
+    @AfterSuite
+    public void cleanup() {
+        if (inputFileWithRemainder != null) {
+            System.out.println("Deleting file : " + inputFileWithRemainder.getAbsolutePath());
+            inputFileWithRemainder.delete();
+        }
 
-        fos.start();
+        if (inputFileWithPrecisePartitionSizeMultiple != null) {
+            System.out.println("Deleting file : " + inputFileWithPrecisePartitionSizeMultiple.getAbsolutePath());
+            inputFileWithPrecisePartitionSizeMultiple.delete();
+        }
     }
 }
